5b84790c9a2debdfa6a0f3bda567ae8a5948f414
Lohan <ltwm1998@gmail.com>
2023-11-13 14:30:34 -0500

    complete lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,27 @@
 # your code goes here!
+
+import time
+
+
+def countdown(int_param):
+    while int_param > 0:
+        if int_param > 1:
+            print(f"{int_param} SECONDS!")
+        elif int_param == 1:
+            print("1 SECOND!")
+        int_param -= 1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(int_param):
+    while int_param > 0:
+        if int_param > 1:
+            time.sleep(1)
+            print(f"{int_param} SECONDS!")
+        elif int_param == 1:
+            time.sleep(1)
+            print("1 SECOND!")
+        int_param -= 1
+    time.sleep(1)
+    print("HAPPY NEW YEAR!")
+        
